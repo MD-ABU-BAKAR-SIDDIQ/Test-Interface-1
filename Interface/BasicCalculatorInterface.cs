d3e643085fe348e3cf8e00c10fd5cd4973f5267b
unknown <siddiqarman66@gmail.com>
2020-11-25 12:19:37 +0600

    Test Interface 1

---
+++ Interface/BasicCalculatorInterface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface BasicCalculatorInterface
+    {
+        int sum(int x, int y);
+        int sub(int x, int y);
+        int multiplication(int x, int y);
+        int division(int x, int y);
+    }
+    class Basicc : BasicCalculatorInterface
+    {
+        int result;
+        public int sum(int num1, int num2)
+        {
+            result = num1 + num2;
+            return result;
+        }
+        public int sub(int num1, int num2)
+        {
+            result = num1 - num2;
+            return result;
+        }
+        public int multiplication(int num1, int num2)
+        {
+            result = num1 * num2;
+            return result;
+        }
+        public int division (int num1, int num2)
+        {
+            result = num1 / num2;
+            return result;
+        }
+        public void showinfo()
+        {
+            Console.WriteLine("Result : " + result);
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" \n   Basic Calculator Interface \n");
+            
+            Basicc b = new Basicc();
+
+            Console.WriteLine("Addition ");
+          
+            b.sum(5, 5);
+            b.showinfo();
+
+            Console.WriteLine("\nSubtraction  ");
+         
+            b.sub(6, 1);
+            b.showinfo();
+
+            Console.WriteLine("\nMultiplication ");
+           
+            b.multiplication(17, 15);
+            b.showinfo();
+
+            Console.WriteLine("\nDivision ");
+           
+            b.division(22, 22);
+            b.showinfo();
+
+        }
+    }
+}
